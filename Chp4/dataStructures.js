afff9ecfa96d77b3652e07141e3f2a24be511ddf
joshua <joshperez199@gmail.com>
2022-02-20 18:34:57 -0500

    test update

--- Chp4/dataStructures.js
+++ Chp4/dataStructures.js
@@ -117,3 +117,13 @@ function rememberUrgently(task) {   //adds task to front of array
 
 console.log([1,2,3,2,1].indexof(2));        //returns index of first 2
 console.log([1,2,3,2,1].lastIndexOf(2));    //returns index of last 2
+
+console.log([0,1,2,3,4].slice(2,4));
+console.log([0,1,2,3,4].slice(2));
+
+function remove(array, index) {
+    return array.slice(0, index).concat(array.slice(index + 1));
+}
+
+console.log(remove(['a', 'b', 'c', 'd', 'e']));
+
